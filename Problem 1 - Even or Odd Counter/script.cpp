65db30574440d025f2b74af902b99b7c775dd28a
Janwin <malinaojanwin2005@gmail.com>
2025-12-07 15:14:41 +0800

    Problem 1 done

--- Problem 1 - Even or Odd Counter/script.cpp
+++ Problem 1 - Even or Odd Counter/script.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
+    int odd = 0;
+    int even = 0;
+    int number;
+    std::cout << "Enter a number(s): " << std::endl;
+    do
+    {
+        std::cin >> number;
+        if (!number)
+            break;
+        number % 2 == 0 ? even++ : odd++;
+    } while (number != 0);
 
+    std::cout << "Even: " << even << " " << "Odd: " << odd;
     return 0;
 }
