4e108b55a46ef49da4c613568c6272e3fce968e0
Janwin <malinaojanwin2005@gmail.com>
2025-12-07 15:31:23 +0800

    Problem 2 done

---
+++ Problem 2 - Grade Classifier/script.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void computeGrades(int);
+
+int main()
+{
+    int grade;
+    std::cout << "Enter Grade: ";
+    std::cin >> grade;
+    computeGrades(grade);
+
+    return 0;
+}
+
+void computeGrades(int grade)
+{
+    if (grade >= 90 && grade <= 100)
+        std::cout << "A";
+    else if (grade >= 80 && grade <= 89)
+        std::cout << "B";
+    else if (grade >= 70 && grade <= 79)
+        std::cout << "C";
+    else if (grade >= 60 && grade <= 69)
+        std::cout << "D";
+    else if (grade > 100 || grade < 0)
+        std::cout << "invalid input";
+    else
+        std::cout << "F";
+}
