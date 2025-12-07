43fe681a504874ef8b84969ba77a28560e8d8555
Janwin <malinaojanwin2005@gmail.com>
2025-12-07 18:19:59 +0800

    Problem 4 done

--- Problem 4 - Count Vowels in a Word (loop + condition)/script.cpp
+++ Problem 4 - Count Vowels in a Word (loop + condition)/script.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
+    std::string word;
+    int vowelCount = 0;
+    std::cout << "Enter word: ";
+    std::cin >> word;
+    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+    for (int i = 0; i < word.length(); i++)
+    {
+        for (char vowel : vowels)
+        {
+            if (std::tolower(word[i]) == vowel)
+                vowelCount++;
+        }
+    }
+    std::cout << vowelCount;
 
     return 0;
 }
