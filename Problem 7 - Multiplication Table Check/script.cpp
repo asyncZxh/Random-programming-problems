e4db8f9bd10aa386340ce89cb5b01e4686eae900
Janwin <malinaojanwin2005@gmail.com>
2025-12-08 16:20:12 +0800

    update Problem 7

--- Problem 7 - Multiplication Table Check/script.cpp
+++ Problem 7 - Multiplication Table Check/script.cpp
@@ -5,17 +5,16 @@ int main()
     int a, b, c;
     int sum = 0;
     int product = 0;
-    bool sign = (a < 0) == (b < 0);
     std::cout << "Enter 3 integers: ";
     std::cin >> a >> b >> c;
+    bool sign = ((a < 0) == (b < 0));
 
     for (int i = 1; i <= std::abs(b); i++)
     {
         sum += std::abs(a);
     }
 
-    !sign ? product = sum : product = -sum;
-
+    sign ? product = sum : product = -sum;
     product == c ? std::cout << "YES" : std::cout << "NO";
 
     return 0;
