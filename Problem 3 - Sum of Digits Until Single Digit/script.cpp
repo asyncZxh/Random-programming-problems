76c0918d1a023594c4ab2e276bbe6c85806be029
Janwin <malinaojanwin2005@gmail.com>
2025-12-08 09:09:57 +0800

    modify Problem 3 script

--- Problem 3 - Sum of Digits Until Single Digit/script.cpp
+++ Problem 3 - Sum of Digits Until Single Digit/script.cpp
@@ -5,18 +5,19 @@ int main()
     long int number;
     std::cout << "Enter a positive number: ";
     std::cin >> number;
-    std::string getNumber = std::to_string(number);
+    std::string convertNumber = std::to_string(number);
 
-    while (getNumber.length() > 1)
+    while (convertNumber.length() > 1)
     {
         long int result = 0;
-        for (int i = 0; i < getNumber.length(); i++)
+        for (int i = 0; i < convertNumber.length(); i++)
         {
-            result += getNumber[i] - '0';
+            result += convertNumber[i] - '0';
         }
-        getNumber = std::to_string(result);
+        convertNumber = std::to_string(result);
     }
-    std::cout << getNumber;
+
+    std::cout << std::stoi(convertNumber);
 
     return 0;
 }
